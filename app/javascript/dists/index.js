7e1164d1d6aa7d78959e034826c1b97257f6e08e
pramod <pramod@Ubuntu18.04>
2021-03-31 20:44:59 +0530

    ACE Theme Integration

---
+++ app/javascript/dists/index.js
@@ -0,0 +1,5 @@
+// Load all the channels within this directory and all subdirectories.
+// Channel files must be named *_channel.js.
+
+const channels = require.context('.', true, /.js$/)
+channels.keys().forEach(channels)
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,12 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+import "@fortawesome/fontawesome-free/js/all";
+
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "dists"
